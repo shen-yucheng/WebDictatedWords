07dfe011bffd8aa7ac4bf1fcbdc079bbf88be564
shen-yucheng <shen28311582@gmail.com>
2021-03-16 21:58:03 +0800

    格式化代码，禁用表单自动填充

--- static/index.js
+++ static/index.js
@@ -1,4 +1,4 @@
-function change_title(){
+function change_title() {
     let obj = document.getElementById("title")
     let title = (obj.value !== "") ? obj.value : "看音写词"
     document.getElementsByTagName('title')[0].innerText = title
